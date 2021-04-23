6454fc97028086386a94ccc1672502d0dc1da615
akashkumar0987d <80807783+akashkumar0987d@users.noreply.github.com>
2021-04-23 14:34:01 +0530

    Update Question 2.c

--- Unit 1/Lab2/Question 2.c
+++ Unit 1/Lab2/Question 2.c
@@ -6,3 +6,13 @@ Step 4:- Calculate sum = f1+f2+f3+f4+f5
 Step 5:- Print sum
 Step 6:- Stop
 */ 
+#include <stdio.h>
+int main()
+{
+    float f1,f2,f3,f4,f4,sum;
+    printf("Enter f1, f2,f3,f4,f5 : ");
+    scanf("%f %f %f %f %f ",&f1, &f2, &f3, &f4, &f5);
+    sum = f1+f2+f3+f4+f5
+    printf("Sum is %f",a,b,sum);
+    return 0;
+}
