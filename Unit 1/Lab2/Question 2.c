d3acd2b9524a8d3d13294f79e7bf5210b099322a
akashkumar0987d <80807783+akashkumar0987d@users.noreply.github.com>
2021-04-23 14:22:18 +0530

    Create Question 2.c

---
+++ Unit 1/Lab2/Question 2.c
@@ -0,0 +1,8 @@
+/* Algorithm:- 
+Step 1:- Start
+Step 2:- Declare Variable f1, f2, f3, f4, f5 and sum as float;
+step 3:- Input f1, f2, f3, f4 and f5
+Step 4:- Calculate sum = f1+f2+f3+f4+f5
+Step 5:- Print sum
+Step 6:- Stop
+*/ 
