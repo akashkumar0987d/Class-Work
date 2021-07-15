0830235c5cc77895ee7b72166043aa4bd21701d8
akashkumar0987d <80807783+akashkumar0987d@users.noreply.github.com>
2021-07-15 19:45:58 +0530

    Create 60.c

---
+++ LIST OF EXPERIMENTS/60.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+struct item
+{
+	char  itemName[30];
+	int qty;
+	float price;
+	float amount;
+};
+
+/*readItem()- to read values of item and calculate total amount*/
+void readItem(struct item *i)
+{
+	printf("Enter product name: ");
+	gets(i->itemName);
+	printf("Enter price:");
+	scanf("%f",&i->price);
+	printf("Enter quantity: ");
+	scanf("%d",&i->qty);
+	i->amount =(float)i->qty * i->price;
+}
+
+/*printItem() - to print values of item*/
+void printItem(struct item *i)
+{
+	printf("\nName: %s",i->itemName);
+	printf("\nPrice: %f",i->price);
+	printf("\nQuantity: %d",i->qty);
+	printf("\nTotal Amount: %f",i->amount);
+
+}
+int main()
+{
+	
+	struct item itm;	
+	struct item *pItem;	
+	
+	pItem = &itm;	
+	readItem(pItem);
+	printItem(pItem);
+	
+	
+	return 0;
+}
+
