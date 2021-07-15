ee96f6b0fb65d83ff560e4133d9781e904788eb6
akashkumar0987d <80807783+akashkumar0987d@users.noreply.github.com>
2021-07-15 14:52:23 +0530

    Create 2.c

---
+++ LIST OF EXPERIMENTS/2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+   printf("Size of int data type : %d\n",sizeof(int));
+   printf("Size of char data type : %d\n",sizeof(char));
+   printf("Size of float data type : %d\n",sizeof(float));
+   printf("Size of double data type : %d\n",sizeof(double));
+   return 0;
+}
+
